edf787ba9c8e74cacbb587bd1710b86863f5ecbd
Mark Welsh <mark.welsh@sparktx.com>
2022-04-26 16:28:20 -0400

    Add username, email and git config cmds

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ SSH_USER_ID="$3"
 SSH_PRIVATE_KEY="$4"
 REPO_NAME="$5"
 BRANCH_NAME="$6"
+SVC_USERNAME="$7"
+SVC_EMAIL="$8"
 
 mkdir -p ~/.ssh && cd ~/.ssh && echo "$SSH_PRIVATE_KEY" > id_rsa && chmod 600 id_rsa;
 echo "Host git-codecommit.*.amazonaws.com
@@ -23,7 +25,9 @@ AWS_RESPONSE=$(aws codecommit get-repository --repository-name "$REPO_NAME" || \
     aws codecommit create-repository --repository-name "$REPO_NAME")
 CODECOMMIT_URL=$(jq -r '.repositoryMetadata.cloneUrlSsh' <<< $AWS_RESPONSE)
 
-echo "$CODECOMMIT_URL"
+git config --global user.email "$SVC_EMAIL"
+git config --global user.name "$SVC_USERNAME"
+git config --global --add safe.directory /github/workspace
 
 cd /github/workspace && \
     git remote add codecommit "$CODECOMMIT_URL" && \
