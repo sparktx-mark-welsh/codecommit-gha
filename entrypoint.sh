1995b43ade80ca5785473ee8594aa89cb4bb79eb
Mark Welsh <mark.welsh@sparktx.com>
2022-04-26 15:29:19 -0400

    Rolling back the export vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,16 +22,9 @@ User $SSH_USER_ID
 IdentityFile ~/.ssh/id_rsa
 StrictHostKeyChecking no" > config && chmod 600 config
 
-cat ~/.ssh/id_rsa || echo "issue with id_rsa"
-cat ~/.ssh/config || echo "issue with config"
-
 aws --profile default configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
 aws --profile default configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 aws --profile default configure set region "us-east-2"
-export AWS_ACCESS_KEY=$AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-export AWS_DEFAULT_REGION=us-east-2
-cat ~/.aws/credentials || echo "issue with aws"
 
 AWS_RESPONSE=$(aws codecommit get-repository --repository-name "$REPO_NAME" || \
     aws codecommit create-repository --repository-name "$REPO_NAME")
