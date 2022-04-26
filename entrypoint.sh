116da6e22ea827e88faa57bf27996562f726abd5
Mark Welsh <mark.welsh@sparktx.com>
2022-04-26 14:20:31 -0400

    debugging entrypoint & test if push works on all branches

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,15 +9,27 @@ SSH_PRIVATE_KEY="{$4}"
 REPO_NAME="{$5}"
 BRANCH_NAME="{$6}"
 
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+echo $SSH_USER_ID
+echo $SSH_PRIVATE_KEY
+echo $REPO_NAME
+echo $BRANCH_NAME
+
 mkdir -p ~/.ssh && cd ~/.ssh && echo "${SSH_PRIVATE_KEY}" > id_rsa && chmod 600 id_rsa;
 echo "Host git-codecommit.*.amazonaws.com
 User ${SSH_USER_ID}
 IdentityFile ~/.ssh/id_rsa
 StrictHostKeyChecking no" > config && chmod 600 config
 
+cat ~/.ssh/id_rsa || echo "issue with id_rsa"
+cat ~/.ssh/config || echo "issue with config"
+
 aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}" && \
 aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}" && \
 aws configure set region "us-east-2";
+
+cat ~/.aws/credentials || echo "issue with aws"
 
 AWS_RESPONSE=$(aws codecommit get-repository --repository-name "${REPO_NAME}" || \
     aws codecommit create-repository --repository-name "${REPO_NAME}")
