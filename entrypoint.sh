31a1c030b025942c30538a65446050e88c5d29a5
Mark Welsh <93157754+sparktx-mark-welsh@users.noreply.github.com>
2022-04-26 23:03:37 -0400

    Debugging GHA live complete; working GitHub Action! (#1)
    * debugging entrypoint & test if push works on all branches
    
    * Remove env context
    
    * Updating action to use latest version on `debug`
    
    * Fix typo
    
    * entry Bash syntax + try AWS CLI vars
    
    * Rolling back the export vars
    
    * Better github event context; removes echo
    
    * Print CODECOMMIT_URL
    
    * cd to checked out code before pushing
    
    * Checkout v3.0.2 to fix safe directory error
    
    * Add username, email and git config cmds
    
    * Listing out directory
    
    * Debugging git steps
    
    * Add known_hosts
    
    * User SSH URL with user-id
    
    * better debugging
    
    * fix git remote cmd
    
    * SSH key debug steps
    
    * SSH verbose
    
    * user debug commands
    
    * multiple ssh confs
    
    * /github/home/ path for SSH files
    
    * Removing debugging lines
    
    * Fetch/checkout the whole branch
    
    this is needed when creating a new repo on CodeCommit -- we can't just
    push the HEAD
    
    * Update YAML syntax for checkout action

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,26 +2,37 @@
 
 set -e
 
-AWS_ACCESS_KEY_ID="{$1}"
-AWS_SECRET_ACCESS_KEY="{$2}"
-SSH_USER_ID="{$3}"
-SSH_PRIVATE_KEY="{$4}"
-REPO_NAME="{$5}"
-BRANCH_NAME="{$6}"
+AWS_ACCESS_KEY_ID="$1"
+AWS_SECRET_ACCESS_KEY="$2"
+SSH_USER_ID="$3"
+SSH_PRIVATE_KEY="$4"
+REPO_NAME="$5"
+BRANCH_NAME="$6"
+SVC_USERNAME="$7"
+SVC_EMAIL="$8"
 
-mkdir -p ~/.ssh && cd ~/.ssh && echo "${SSH_PRIVATE_KEY}" > id_rsa && chmod 600 id_rsa;
+mkdir -p /github/home/.ssh && cd /github/home/.ssh && echo "$SSH_PRIVATE_KEY" > id_rsa && chmod 600 id_rsa;
+
 echo "Host git-codecommit.*.amazonaws.com
-User ${SSH_USER_ID}
-IdentityFile ~/.ssh/id_rsa
-StrictHostKeyChecking no" > config && chmod 600 config
+User $SSH_USER_ID
+IdentityFile /github/home/.ssh/id_rsa
+StrictHostKeyChecking no" > /etc/ssh/ssh_config.d/codecommit.conf
 
-aws configure set aws_access_key_id "${AWS_ACCESS_KEY_ID}" && \
-aws configure set aws_secret_access_key "${AWS_SECRET_ACCESS_KEY}" && \
-aws configure set region "us-east-2";
+ssh-keyscan -t rsa -H git-codecommit.us-east-2.amazonaws.com > known_hosts && chmod 600 known_hosts
 
-AWS_RESPONSE=$(aws codecommit get-repository --repository-name "${REPO_NAME}" || \
-    aws codecommit create-repository --repository-name "${REPO_NAME}")
-CODECOMMIT_URL=$(jq -r '.repositoryMetadata.cloneUrlSsh' <<< ${AWS_RESPONSE})
+aws --profile default configure set aws_access_key_id "$AWS_ACCESS_KEY_ID"
+aws --profile default configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
+aws --profile default configure set region "us-east-2"
 
-git remote add codecommit "$CODECOMMIT_URL"
-git push codecommit ${BRANCH_NAME}
+AWS_RESPONSE=$(aws codecommit get-repository --repository-name "$REPO_NAME" || \
+    aws codecommit create-repository --repository-name "$REPO_NAME")
+CLONE_URL=$(jq -r '.repositoryMetadata.cloneUrlSsh' <<< $AWS_RESPONSE)
+CODECOMMIT_URL="ssh://${SSH_USER_ID}@${CLONE_URL:6}" # trim off the first 6 chars "ssh://", add user-id, add ssh:// back
+
+git config --global user.email "$SVC_EMAIL"
+git config --global user.name "$SVC_USERNAME"
+git config --global --add safe.directory /github/workspace
+
+cd /github/workspace && \
+    git remote add codecommit "$CODECOMMIT_URL" && \
+    GIT_SSH_COMMAND="ssh -vv" git push codecommit $BRANCH_NAME
